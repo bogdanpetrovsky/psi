9d081161b4e565d81dc34d3cee92f7f27a94b36f
bogdanpetrovsky <bpetrovsky977@gmail.com>
2023-04-06 12:53:44 +0300

    got unfiltered news headings

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,22 @@
-console.log('Hello world!')
+import { chromium } from '@playwright/test';
+import assert from 'assert';
+
+(async () => {
+  // Setup
+  const browser = await chromium.launch();
+  const context = await browser.newContext();
+  const page = await context.newPage();
+
+  // The actual interesting bit
+  await context.route('**.jpg', (route: any )=> route.abort());
+  await page.goto('https://www.pravda.com.ua/news/');
+  console.log(await page.title());
+  const text = await page.getByRole('link').allInnerTexts();
+  console.log(text);
+
+
+  assert(true); // 👎 not a Web First assertion
+
+  await context.close();
+  await browser.close();
+})()
